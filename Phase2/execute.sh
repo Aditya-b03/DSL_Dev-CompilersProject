3490b2439d6bef0d6ac23f03ab20d9b3282b5b48
aditya bacharwar <adityabacharwar@192.168.1.13>
2023-10-29 19:10:54 +0530

    Updated script

--- Phase2/execute.sh
+++ Phase2/execute.sh
@@ -1,8 +1,33 @@
+#!/bin/bash
+# Check if the path to the input file is provided as an argument
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <path_to_input_file>"
+    exit 1
+fi
 
-
+# Path to the input file
 input_file="$1"
+
+# Check if the input file exists
+if [ ! -f "$input_file" ]; then
+    echo "Input file '$input_file' does not exist."
+    exit 1
+fi
+
+# Execute the commands in order
+
+# Step 1: Run Flex (lex.l)
 flex lex.l
+# Step 2: Run Yacc (yacc.y)
 yacc -d yacc.y
-gcc lex.yy.c y.tab.c
+# Step 3: Compile the generated code
+gcc lex.yy.c y.tab.c -o a.out
+# Step 4: Execute the generated program with the input file
 ./a.out "$input_file"
 
+# Check if the execution was successful
+if [ $? -eq 0 ]; then
+    echo "Execution completed successfully."
+else
+    echo "Execution encountered an error."
+fi
