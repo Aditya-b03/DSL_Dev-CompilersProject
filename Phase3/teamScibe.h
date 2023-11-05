ea829d8e7d8d11e9d14e703c244439fb273ed5de
Aditya Bacharwar <es21btech11003@iith.ac.in>
2023-11-05 11:48:42 +0530

    header updates

--- Phase3/teamScibe.h
+++ Phase3/teamScibe.h
@@ -8,8 +8,38 @@ class team;
 class member;
 class task;
 class calendar{};
-class date{};
 
+typedef struct date{
+    int day;
+    int month;
+    int year;
+
+    date(int day = 0, int month = 0, int year = 0){
+        if(day > 31 || day < 1 || month > 12 || month < 1 || year < 0){
+            cout << "Error: Invalid Date" << endl;
+            return;
+        }
+        this->day = day;
+        this->month = month;
+        this->year = year;
+    }
+
+
+    
+
+}date;
+
+
+string dateToString(date d)
+{
+    string s = "";
+    s += to_string(d.day);
+    s += "-";
+    s += to_string(d.month);
+    s += "-";
+    s += to_string(d.year);
+    return s;
+}
 
 string gen_ID(){
     return "random ID"; // future me will implement this
@@ -30,10 +60,10 @@ public:
 
     // team member functions
     team(string name = "random name", member lead = *DEFAULT_LEAD,
-    string moto = "No Description Given", vector<member> members = {}, vector<team> sub_teams = {});
+    string moto = "No Description Given", vector<member *> members = {}, vector<team *> sub_teams = {});
 };
 
-team::team(string name = "random name", member lead = *DEFAULT_LEAD, string moto = "No Description Given", vector<member> members = {}, vector<team> sub_teams = {})
+team::team(string name = "random name", member lead = *DEFAULT_LEAD, string moto = "No Description Given", vector<member *> members = {}, vector<team *> sub_teams = {})
 {   
     this->team_id = gen_ID();
     this->name = name;
@@ -41,14 +71,16 @@ team::team(string name = "random name", member lead = *DEFAULT_LEAD, string moto
     this->description = moto;
     for (int i = 0; i < members.size(); i++)
     {
-        this->members.insert(&members[i]);
+        this->members.insert(members[i]);
     }
 
     for (int i = 0; i < sub_teams.size(); i++)
     {
-        this->sub_teams.insert(&sub_teams[i]);
+        this->sub_teams.insert(sub_teams[i]);
     }
 }
+
+
 
 // add_member_function -> two type of args -> single member or vector of members // function overloading
 // will add member to the team and // later me problem : update the team's calendar if required
@@ -96,21 +128,57 @@ class task{
     string task_id;
     set<member *> assigned_to; // set of pointers to members
     int priority;
-
 public:
     string title;
     string description;
     string status;
     date due_date;
-    
+    task(string title = "random title", string description = "No Description Given", int priority = 0, string status = "To Do", date due_date = date());
+};
+
+// code task member functions
+task::task(string title = "random title", string description = "No Description Given", int priority = 0, string status = "To Do", date due_date = date())
+{
+    this->task_id = gen_ID();
+    this->title = title;
+    this->description = description;
+    this->priority = priority;
+    this->status = status;
+    this->due_date = due_date;
+}
 
 
-
-
-
+class calander{
+    map<string, set<task *>> calander_map;
+    public:
+    void add_task(task &t, date d);
+    void remove_task(task &t, date d);
+    void display(date d);
 };
 
 
+void calander::add_task(task &t, date d)
+{
+    this->calander_map[dateToString(d)].insert(&t);
+}
+
+// proper return values 1 - success, -1 - task not found
+void calander::remove_task(task &t, date d)
+{
+    this->calander_map[dateToString(d)].erase(&t);
+}
+
+
+// make this function more proper
+void calander::display(date d)
+{
+    set<task *> tasks = this->calander_map[dateToString(d)];
+    for (auto i = tasks.begin(); i != tasks.end(); i++)
+    {
+        cout << "title: " <<(*i)->title << endl;    //check what to display
+    }
+    
+}
 
 
 
